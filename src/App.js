6b4fb9ec0431f0aecca0f35908449da1a8ed9a14
Randi Schreiner <mimirandi21@gmail.com>
2021-02-19 14:14:28 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { addItem } from "./actions/items";
 
 class App extends Component {
+	handleOnClick = (event) => {
+		this.props.addItem();
+	};
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
-        <p>{this.props.items.length}</p>
-      </div>
-    );
-  }
-};
+	render() {
+		return (
+			<div className="App">
+				<button onClick={(event) => this.handleOnClick(event)}>Click</button>
+				<p>{this.props.items.length}</p>
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
+	return {
+		items: state.items,
+	};
 };
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		addItem: () => {
+			dispatch(addItem());
+		},
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
